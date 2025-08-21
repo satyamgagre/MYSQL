60f5922873ca50815d32897f5fdb9069f939fd62
Satyam Gagare <satyamgagre4@gmail.com>
2025-08-21 22:30:13 +0530

    21-08-2025.

---
+++ DELETE_Removing_Data_from_a_Table.sql
@@ -0,0 +1,31 @@
+USE startersql;
+-- DELETE FROM users
+-- WHERE salary < 66000;
+
+-- DELETE FROM users
+-- WHERE id =1;
+
+--  Delete One Row
+-- DELETE FROM users 
+-- WHERE id = 3;
+
+-- Delete Multiple Rows
+-- DELETE FROM users
+-- WHERE gender = 'Other';
+
+-- Delete All Rows
+-- DELETE FROM users;
+
+-- Drop the Entire Table
+-- DROP TABLES FROM users;
+
+-- Delete users Salary less than 50000
+-- DELETE FROM users
+-- WHERE salary < 50000;
+
+-- Delete users Salary is NULL 
+-- DELETE FROM users
+-- WHERE salary IS NULL;
+
+
+SELECT * FROM users;
